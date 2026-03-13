dafa9ac0fba6b3d80ae7c256ff3340181481dc38
DennysMendonca <dennys.charlles.silva.mendonca@alunoedufor.com.br>
2026-03-12 23:40:23 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-/*const formulario = document.getElementById('meuForm');
-
-if (formulario) {
-    formulario.addEventListener('submit', function(event) {
-        event.preventDefault(); // Impede o recarregamento da página
-
-        const inputQuantidade = document.getElementById('quantidade');
-        const quantidade = parseInt(inputQuantidade.value);
-
-        if (quantidade > 0) {
-            const resultado = Math.floor(Math.random() * quantidade) + 1;
-            alert("🎊O aluno sorteado foi: " + resultado);
-        } else {
-            alert("Por favor, digite um número de alunos válido.");
-        }
-        body.style.backgroudColor = "#FFC107";
-        document.getElementById("cabecalho").style.backgroudColor = "#333";
-        }
-    }};
-} else {
-    console.error("Formulário não encontrado! Verifique se o ID no HTML é 'meuForm'");
-}
-*/
 
 // Pegando os elementos (Aula 4)
 const formulario = document.getElementById("meuForm");
